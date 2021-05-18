301493dc4444a2f506d11d5641028388ac2fb519
Alex <alexander.salvatore@gmail.com>
2021-05-18 16:11:44 +0200

    added Bank to the index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Transaction from "./src/transaction.js";
 import Blockchain from "./src/blockchain.js";
 import Wallet from "./src/wallet.js";
 import Block from "./src/block.js";
+import Bank from "./src/bank.js";
 
 // without default
-export { Transaction, Blockchain, Wallet, Block };
-
+export { Transaction, Blockchain, Wallet, Block, Bank };
